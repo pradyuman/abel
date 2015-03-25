4a5b4772d1e338e12ff12488656bcd5ee6a66fc9
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-03-25 19:50:17 -0400

    Added simple decoder

--- assets/js/userFunctions.js
+++
@@ -1,9 +0,0 @@
-$("#aboutButton").click(function() {
-    $("#documents").css("display","none");
-    $("#about").css("display","inline");
-});
-
-$("#docButton").click(function() {
-    $("#about").css("display","none");
-    $("#documents").css("display","inline");
-});
